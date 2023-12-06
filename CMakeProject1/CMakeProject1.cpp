505712db335310bb8d64e8d5604a756eb7cd15d0
257224 <145651907+257224@users.noreply.github.com>
2023-12-06 14:20:25 +0100

    projekt kino1.cpp

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -4,6 +4,10 @@
 #define ROWS 5
 #define COLS 5
 
+struct Film {
+    char name[50];
+    char seats[ROWS][COLS];
+};
 
 void displaySedatek(char seats[ROWS][COLS]) {
     printf("\n\nVolne mista:\n\n");
@@ -18,17 +22,32 @@ void displaySedatek(char seats[ROWS][COLS]) {
     printf("\n");
 }
 
-
-void vyberKina() {
+void vyberKina(struct Film *film) {
     printf("Vyberte film:\n");
     printf("1. Spider-man 3 \n");
     printf("2. Harry Potter \n");
     printf("3. 3 orisky pro popelku\n");
-   
+
+    int choice;
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            strcpy(film->name, "Spider-man 3");
+            break;
+        case 2:
+            strcpy(film->name, "Harry Potter");
+            break;
+        case 3:
+            strcpy(film->name, "3 orisky pro popelku");
+            break;
+        default:
+            printf("Neplatny vyber filmu.\n");
+            break;
+    }
 }
 
-
-void rezervace(char seats[ROWS][COLS]) {
+void rezervace(struct Film *film) {
     int row, col;
 
     printf("Vyberte radek (1-%d): ", ROWS);
@@ -37,30 +56,30 @@ void rezervace(char seats[ROWS][COLS]) {
     printf("Vyberte cislo mista (1-%d): ", COLS);
     scanf("%d", &col);
 
-    
-    if (row >= 1 && row <= ROWS && col >= 1 && col <= COLS && seats[row - 1][col - 1] == 'O') {
-        seats[row - 1][col - 1] = 'X';
-        printf("Misto mate rezervovano!\n");
+    if (row >= 1 && row <= ROWS && col >= 1 && col <= COLS && film->seats[row - 1][col - 1] == 'O') {
+        film->seats[row - 1][col - 1] = 'X';
+        printf("Misto mate rezervovano pro film \"%s\"!\n", film->name);
     } else {
-        printf("Omlouvame se, misto uz je obsazeno.\n");
+        printf("Omlouvame se, misto uz je obsazeno nebo neplatny vyber filmu.\n");
     }
 }
 
 int main() {
-    char seats[ROWS][COLS]; 
+    struct Film film;
+    strcpy(film.name, "");
 
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            seats[i][j] = 'O'; 
+            film.seats[i][j] = 'O';
         }
     }
 
     int choice;
 
     do {
-        vyberKina(); 
-        displaySedatek(seats); 
-        rezervace(seats); 
+        vyberKina(&film);
+        displaySedatek(film.seats);
+        rezervace(&film);
 
         printf("Chcete pokracovat? (1 - Ano, 0 - Ne): ");
         scanf("%d", &choice);
